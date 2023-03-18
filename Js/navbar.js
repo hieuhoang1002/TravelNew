1c3922759019d9dd66e11559db70e73066a84e50
HieuHoang102 <hieuhoang102nqa@gmail.com>
2023-03-18 17:32:42 +0700

    update

--- Js/navbar.js
+++ Js/navbar.js
@@ -14,23 +14,3 @@ function scrollFunction() {
   }
 }
 // ================================================================
-function scrollItemFunction() {
-  if (
-    document.body.scrollTop > 440 ||
-    document.documentElement.scrollTop > 400
-  ) {
-    document.getElementById("scrollItem").style.opacity = "1";
-  } else {
-    document.getElementById("scrollItem").style.opacity = "0";
-  }
-}
-// ================================================================
-
-// ================================================================
-const scrollItem = document.getElementById("scrollItem");
-scrollItem.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: `smooth`,
-  });
-});
