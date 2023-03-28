3ad24f3b8e62d87b39bc235f1ee31706e65a5819
HieuHoang102 <hieuhoang102nqa@gmail.com>
2023-03-28 13:46:39 +0700

    update

--- Js/responOwlcarousel.js
+++ Js/responOwlcarousel.js
@@ -3,6 +3,9 @@ $(".owl-carousel").owlCarousel({
   margin: 10,
   items: 2,
   responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
   responsive: {
     0: {
       items: 1,
